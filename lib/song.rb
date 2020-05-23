bd64ef356fe76190775bdf5afa91759a0a098c7c
Tommy Werner <wtwerner@gmail.com>
2020-05-23 20:40:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
-      self.name
+      self.find_by_name(name)
     else
       self.create_by_name(name)
     end
